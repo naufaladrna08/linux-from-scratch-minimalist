a05ab34dad98f1259cc0b8bf69cad1a8a6c20c08
naufaladrna08 <naufaladrn90@gmail.com>
2021-07-17 18:51:50 +0700

    ditambahkan konfigurasi file system table

--- config.sh
+++ config.sh
@@ -25,3 +25,14 @@ video:x:12:
 utmp:x:13:
 usb:x:14:
 EOF
+echo "Creating /etc/passwd..."
+cat > ${MLFS_ROOT}/etc/fstab << "EOF"
+# file system  mount-point  type   options          dump  fsck
+#                                                         order
+
+rootfs          /               auto    defaults        1      1
+proc            /proc           proc    defaults        0      0
+sysfs           /sys            sysfs   defaults        0      0
+devpts          /dev/pts        devpts  gid=4,mode=620  0      0
+tmpfs           /dev/shm        tmpfs   defaults        0      0
+EOF"
