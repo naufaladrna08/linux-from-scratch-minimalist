59077cc585991b252599a7c5cc6569b6473b0fbd
naufaladrna08 <naufaladrn90@gmail.com>
2021-07-17 23:28:39 +0700

    dipindahin dari sources ke root. salah nyimpen

---
+++ cross-compiler.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Konfirmasi
+echo "Cross Compiler adalah compiler yang mampu menciptakan kode dieksekusi untuk platform lain dari yang di mana compiler berjalan. Contohnya anda mengkompilasi program dengan cross compiler di Linux untuk Windows."
+
+# Unset C Flags
+# echo "Unsetting C Flags"
+# unset CXXFLAGS
+# unset CFLAGS
+
