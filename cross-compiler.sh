e4874fb7a68d4256d56d6dd80f80eb484b5c27a3
naufaladrna08 <naufaladrn90@gmail.com>
2021-07-18 06:02:24 +0700

    ditambahkan binutils

--- cross-compiler.sh
+++ cross-compiler.sh
@@ -32,4 +32,22 @@ echo "Done"
 cd linux-4.16.3
 make ARCH=${T_ARCH} headers_check && make ARCH=${T_ARCH} INSTALL_HDR_PATH=dest headers_install
 cp -r dest/include/* ${TLFS_ROOT}/usr/include
+# End of Kernel headers
 
+# Binutils
+echo "Compiling binutils"
+
+echo "Extracting binutils, please wait..."
+# tar -xf sources/binutils-2.30.tar.xz
+
+mkdir binutils-build
+cd binutils-build
+
+../binutils-2.30/configure --prefix=${TLFS_ROOT}/xtools --target=${T_TARGET} --with-sysroot=${TLFS_ROOT} --disable-nls --enable-shared --disable-multilib
+make configure-host && make
+ln -sv lib ${TLFS_ROOT}/xtools/lib64
+make install
+cp -v ../binutils-2.30/include/libiberty.h ${TLFS_ROOT}/usr/include
+
+echo "Done"
+# End of binutils
