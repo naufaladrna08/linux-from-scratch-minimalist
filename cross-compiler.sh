9ffdb23bcee775506c0555c8abfe14c17ccf71bd
naufaladrna08 <naufaladrn90@gmail.com>
2021-07-18 01:57:50 +0700

    ditambahkan compile kernel header

--- cross-compiler.sh
+++ cross-compiler.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+TLFS_ROOT=`cat root`
+
 # Konfirmasi
 echo "| Cross Compiler adalah compiler yang mampu menciptakan kode   |"
 echo "| yang eksekusi untuk platform lain dari yang di mana compiler |"
@@ -19,3 +21,15 @@ export T_TARGET=x86_64-unkown-linux-gnu
 export T_CPU=k8 
 export T_ARCH=$(echo ${T_TARGET} | sed -e 's/-.*//' -e 's/i.86/i386/')
 export T_ENDIAN=little
+
+# Kernel Headers
+echo "Installing kernel headers..."
+
+echo "Extracting linux source, please wait"
+tar -xf sources/linux-4.16.3.tar.xz
+echo "Done"
+
+cd linux-4.16.3
+make ARCH=${T_ARCH} headers_check && make ARCH=${T_ARCH} INSTALL_HDR_PATH=dest headers_install
+cp -r dest/include/* ${TLFS_ROOT}/usr/include
+
