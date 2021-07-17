7eff937bc504f030193411a521454d6bb8ac384a
naufaladrna08 <naufaladrn90@gmail.com>
2021-07-18 01:21:05 +0700

    ditambahkan host dan variabel

--- cross-compiler.sh
+++ cross-compiler.sh
@@ -6,8 +6,16 @@ echo "| yang eksekusi untuk platform lain dari yang di mana compiler |"
 echo "| berjalan. Contohnya anda mengkompilasi program dengan cross  |"
 echo "| compiler di Linux untuk Windows."
 
+echo "Pastikan anda sudah menjalankan packages.sh"
+
 # Unset C Flags
 echo "Unsetting C Flags"
 unset CXXFLAGS
 unset CFLAGS
 
+# Define host and target variables
+export T_HOST=$(echo ${MACHTYPE} | sed "s/-[^-]*/-cross/") 
+export T_TARGET=x86_64-unkown-linux-gnu
+export T_CPU=k8 
+export T_ARCH=$(echo ${T_TARGET} | sed -e 's/-.*//' -e 's/i.86/i386/')
+export T_ENDIAN=little
