b99c8e5d55a48c1ad7737015fd84274b987b2509
naufaladrna08 <naufaladrn90@gmail.com>
2021-07-18 07:36:02 +0700

    ditambahkan zlib dan bootscript

--- target-image.sh
+++ target-image.sh
@@ -53,3 +53,45 @@ cp -v System.map ${TLFS_ROOT}/boot/System.map-4.16.3
 cp -v .config ${TLFS_ROOT}/boot/config-4.16.3
 
 ${TLFS_ROOT}/xtools/bin/depmod.pl -F ${TLFS_ROOT}/boot/System.map-4.16.3 -b ${TLFS_ROOT}/lib/modules/4.16.3
+
+# End of Linux Kernel
+
+cd ..
+
+# CLFS Bootscript
+
+echo "-> CLFS Embedded Bootscript"
+
+echo "Extracting bootscript, please wait..."
+tar -xf sources/clfs-embedded-bootscripts-1.0-pre5.tar.bz2
+
+echo "Compiling bootscripts, please wait..."
+cd clfs-embedded-bootscripts-1.0-pre5
+make DESTDIR=${TLFS_ROOT}/ install-bootscripts
+ln -sv ../rc.d/startup ${TLFS_ROOT}/etc/init.d/rcS
+
+# End of Bootscript
+
+cd ..
+
+# Zlib
+
+echo "-> Zlib"
+
+echo "Extracting zlib, please wait..."
+tar -xf sources/zlib-1.2.11.tar.gz
+cd zlib-1.2.11
+
+echo "Compiling zlib, please wait..."
+sed -i 's/-O3/-Os/g' configure
+./configure --prefix=/usr --shared
+make && make DESTDIR=${TLFS_ROOT}/ install
+
+mv -v ${TLFS_ROOT}/usr/lib/libz.so.* ${TLFS_ROOT}/lib
+ln -svf ../../lib/libz.so.1 ${TLFS_ROOT}/usr/lib/libz.so
+ln -svf ../../lib/libz.so.1 ${TLFS_ROOT}/usr/lib/libz.so.1
+ln -svf ../lib/libz.so.1 ${TLFS_ROOT}/lib64/libz.so.1
+
+# End of zlib
+
+cd ..
