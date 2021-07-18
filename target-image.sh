458f366b1b9fe43c39481cd3eebadff0e9f0c4cc
naufaladrna08 <naufaladrn90@gmail.com>
2021-07-18 07:23:12 +0700

    ditambahkan linux kernel

--- target-image.sh
+++ target-image.sh
@@ -15,7 +15,7 @@ export T_ENDIAN=little
 
 # BusyBox
 
-echo "Compiling BusyBox"
+echo "-> BusyBox"
 
 echo "Extracting BusyBox, please wait..."
 tar -xf sources/busybox-1.28.3.tar.bz2
@@ -30,3 +30,26 @@ cp -v examples/depmod.pl ${TLFS_ROOT}/xtools/bin
 chmod 755 ${TLFS_ROOT}/xtools/bin/depmod.pl
 
 # End of BusyBox
+
+cd ..
+
+# Linux Kernel
+
+echo "-> Linux"
+
+echo "Entering Linux directory, please wait..."
+cd linux-4.16.3
+
+echo "Compiling Linux with default config, please wait..."
+
+make ARCH=${T_ARCH} CROSS_COMPILE=${T_TARGET}- x86_64_defconfig
+make ARCH=${T_ARCH} CROSS_COMPILE=${T_TARGET}-
+make ARCH=${T_ARCH} CROSS_COMPILE=${T_TARGET}- INSTALL_MOD_PATH=${TLFS_ROOT} modules_install
+
+echo "Copying necessary files"
+
+cp -v arch/x86/boot/bzImage ${TLFS_ROOT}/boot/vmlinuz-4.16.3
+cp -v System.map ${TLFS_ROOT}/boot/System.map-4.16.3
+cp -v .config ${TLFS_ROOT}/boot/config-4.16.3
+
+${TLFS_ROOT}/xtools/bin/depmod.pl -F ${TLFS_ROOT}/boot/System.map-4.16.3 -b ${TLFS_ROOT}/lib/modules/4.16.3
